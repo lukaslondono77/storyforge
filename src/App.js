cf279eb925e01421982dfde5f7b5f71cf94a409e
lukaslondono77 <lukaslondono77@github.com>
2026-04-20 14:49:27 -0500

    Wrap application with I18nProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./lib/auth";
+import { I18nProvider } from "./lib/i18n";
 import Header from "./components/Header";
 import Discover from "./pages/Discover";
 import StoryDetail from "./pages/StoryDetail";
@@ -12,24 +13,26 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <div className="app">
-          <Header />
-          <main className="main">
-            <Routes>
-              <Route path="/"            element={<Discover />}   />
-              <Route path="/story/:slug" element={<StoryDetail />} />
-              <Route path="/write"       element={<Write />}       />
-              <Route path="/edit/:slug"  element={<Write />}       />
-              <Route path="/dashboard"   element={<Dashboard />}   />
-              <Route path="/plans"       element={<Plans />}       />
-              <Route path="/register"    element={<Register />}    />
-              <Route path="/login"       element={<Login />}       />
-            </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
-    </AuthProvider>
+    <I18nProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <div className="app">
+            <Header />
+            <main className="main">
+              <Routes>
+                <Route path="/"            element={<Discover />}    />
+                <Route path="/story/:slug" element={<StoryDetail />} />
+                <Route path="/write"       element={<Write />}       />
+                <Route path="/edit/:slug"  element={<Write />}       />
+                <Route path="/dashboard"   element={<Dashboard />}   />
+                <Route path="/plans"       element={<Plans />}       />
+                <Route path="/register"    element={<Register />}    />
+                <Route path="/login"       element={<Login />}       />
+              </Routes>
+            </main>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
+    </I18nProvider>
   );
 }
